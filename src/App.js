5d42db63281dfe02dd798cec17abac07984ccd6b
mlisivick <matt2014!>
2018-04-12 22:01:59 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.app}>
+                <View style={styles.appHeader}>
+                    <Text style={styles.appTitle}>Welcome to React ⚛️</Text>
+                </View>
+                <Text style={styles.appIntro}>
+                    To get started, edit src/App.js and save to reload.
+                </Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    app: {
+        flex: 1
+    },
+    appHeader: {
+        flex: 1,
+        backgroundColor: '#222',
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    appTitle: {
+        fontSize: 16,
+        color: 'white'
+    },
+    appIntro: {
+        flex: 2,
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
